19d2d3a1a287ece898d83da6aab35ff5d494ea1c
ccolten3 <156143216+ccolten3@users.noreply.github.com>
2024-02-26 18:43:19 -0800

    updateed core32

--- core.sv
+++ core.sv
@@ -1,15 +1,55 @@
-core32(
+module core32(
 	input logic clk,
 	input logic reset,
 	input logic reset_pc,
-	input logic inst_mem_req,
-	input logic inst_mem_rsp,
+	output inst_mem_req,
+	input inst_mem_rsp,
 	input logic inst_mem_req_ack,
 
-	output logic data_mem_req,
-	output logic data_mem_rsp,
+	output data_mem_req,
+	input data_mem_rsp,
 	output logic data_mem_req_ack
     );
+
+	logic [31:0] PC_in, PC_out, instr, write_data, read_data1, read_data2;
+	logic [31:0] immediate_gen, ALU_mux_in;
+	logic Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite, PC4toReg;
+	logic [1:0] ALUOp;
+	
+	//functions for determining instruction formating from risc32_starter.sv
+	assign instr = inst_mem_rsp.data; // define instructions
+	
+	opcode_q opcodeq = decode32_opcode_q(instr); //define opcode_q
+	
+	instr_format format = decode32_format(opcodeq);// define format
+	
+	
+	//program counter
+	program_counter PC (.clk, .reset, .PC_in, .PC_out);
+	assign inst_mem_req.addr = PC_out;
+	
+	
+	//registers
+	registers registers(.clk, .reset, .read_reg1(inst[19:15]), .read_reg2(intr[24:20]), .write_reg(instr[11:7]), 
+								.write_data, .RegWrite, .read_data1, .read_data2);
+	
+	//Immediate Generation
+	assign immediate_gen = decode32_imm(instr, format);
+	
+	//control logic
+	control control(.opcode(instr[6:0]), .Branch, .MemRead, .MemtoReg, .ALUOp, .MemWrite, .ALUSrc, .RegWrite,.PC4toReg);
+	
+	// muxed registers output
+	mux2_1_32 reg_mux (.A(immediate_gen), .B(read_data2), .sel(ALUSrc), .out(ALU_mux_in));
+	
+	//ALU control logic
+	ALU_control AlU_cont(.ALUOp, func7, func3, ALU_control);
+
+
+
+
+
+
 endmodule
 
 	
