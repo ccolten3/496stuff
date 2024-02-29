f90d43459b3d750d75ec2d9529e53f4aa01fd9fa
ccolten3 <156143216+ccolten3@users.noreply.github.com>
2024-02-29 14:53:47 -0800

    updated control

--- control.sv
+++ control.sv
@@ -1,29 +1,32 @@
-module control(opcode, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite,PC4toReg);
-    input logic [6:0] opcode;
-    output logic Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite;
-    output logic [1:0] ALUOp;
-
-    instr_format format = decode32_format(opcode[6:2]);
+`include "riscv32_starter.sv"
+module control(instr, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite,PC4toReg);
+    input logic [31:0] instr;
+    output logic Branch, MemtoReg, ALUSrc, RegWrite, PC4toReg;
+    output logic [3:0] MemRead, MemWrite;
+	 output logic [1:0] ALUOp;
+	
+	 opcode_q opcode = decode32_opcode_q(instr);
+	 instr_format format = decode32_format(opcode);
 
     always_comb begin 
         case(format)
         r_format: begin 
-                    Branch = 0;
-                    MemRead = 0;
-                    MemtoReg = 0;
-                    MemWrite = 0;
-                    ALUSrc = 0;
-                    RegWrite = 1;
+                    Branch = 1'b0;
+                    MemRead = 4'b0000;
+                    MemtoReg = 1'b0;
+                    MemWrite = 4'b0000;
+                    ALUSrc = 1'b0;
+                    RegWrite = 1'b1;
                     ALUOp = 2'b10;
-                    PC4toReg = 0;
+                    PC4toReg = 1'b0;
                 end 
         i_format: begin 
                     case(opcode) 
-                        7'b0000011: begin // lw/lb
+                        7'b0000011: begin // lw
                             Branch = 0;
-                            MemRead = 1;
+                            MemRead = 4'b1111;
                             MemtoReg = 1;
-                            MemWrite = 0;
+                            MemWrite = 4'b0000;
                             ALUSrc = 1;
                             RegWrite = 1;
                             ALUOp = 2'b00;
@@ -31,9 +34,9 @@ module control(opcode, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWr
                         end
                         7'b0010011: begin //addi
                             Branch = 0;
-                            MemRead = 0;
+                            MemRead = 4'b0000;
                             MemtoReg = 0;
-                            MemWrite = 0;
+                            MemWrite = 4'b0000;
                             ALUSrc = 1;
                             RegWrite = 1;
                             ALUOp = 2'b00;
@@ -45,9 +48,9 @@ module control(opcode, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWr
                     case(opcode) 
                         7'b0100011: begin 
                             Branch = 0;
-                            MemRead = 0;
+                            MemRead = 4'b0000;
                             MemtoReg = 1'bx;
-                            MemWrite = 1;
+                            MemWrite = 4'b1111;
                             ALUSrc = 1;
                             RegWrite = 0;
                             ALUOp = 2'b00;
@@ -55,9 +58,9 @@ module control(opcode, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWr
                         end
                         default: begin 
                             Branch = 0;
-                            MemRead = 0;
+                            MemRead = 4'b0000;
                             MemtoReg = 1'bx;
-                            MemWrite = 1;
+                            MemWrite = 4'b1111;
                             ALUSrc = 1;
                             RegWrite = 0;
                             ALUOp = 2'b00;
@@ -67,28 +70,29 @@ module control(opcode, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWr
                 end
         b_format: begin //beq 
                     Branch = 1;
-                    MemRead = 0;
+                    MemRead = 4'b0000;
                     MemtoReg = 1'bx;
-                    MemWrite = 0;
+                    MemWrite = 4'b0000;
                     ALUSrc = 0;
                     RegWrite = 0;
                     ALUOp = 2'b01;
                     PC4toReg = 0;
                 end
-        //j_format: begin //jal
-                  //  Branch = 0;
-                  //  MemRead = 0;
-                  //  MemtoReg = 0;
-                  //  MemWrite = 0;
-                  //  ALUSrc = 1'bx;
-                  //  RegWrite = 1;
-                  //  ALUOp = 2'bxx;
-                //end 
+        j_format: begin //jal
+                    Branch = 0;
+                    MemRead = 4'b0000;
+                    MemtoReg = 0;
+                    MemWrite = 4'b0000;
+                    ALUSrc = 1'bx;
+                    RegWrite = 1;
+                    ALUOp = 2'bxx;
+						  PC4toReg = 1;
+                end 
         default:  begin 
                     Branch = 0;
-                    MemRead = 0;
+                    MemRead = 4'b0000;
                     MemtoReg = 0;
-                    MemWrite = 0;
+                    MemWrite = 4'b0000;
                     ALUSrc = 0;
                     RegWrite = 1;
                     ALUOp = 2'b10;
@@ -99,4 +103,3 @@ endmodule
         
 
         
-
--- program_counter.sv
+++ program_counter.sv
@@ -1,12 +1,14 @@
-module program_counter(clk, reset, PC_in, PC_out);
+module program_counter(clk, reset, reset_pc, PC_in, PC_out, valid);
 	input logic clk, reset;
-	input logic [31:0] PC_in;
+	input logic [31:0] PC_in, reset_pc;
 	output logic [31:0] PC_out;
+	output logic valid;
 	
 	always @(posedge clk) begin 
-		if(reset)
-			PC_out <= 32'h00000000;
-		else 
-			PC_out <= PC_in
+		if(reset) begin
+			PC_out <= reset_pc;
+			valid <= 1'b1;
+		end else 
+			PC_out <= PC_in;
 	end
 endmodule 
