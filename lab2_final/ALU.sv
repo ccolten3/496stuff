5921dcb70b5fec0c0db895c4bf9bc2d7e588faad
ccolten3 <156143216+ccolten3@users.noreply.github.com>
2024-03-12 22:29:33 -0700

    Update ALU.sv

--- lab2_final/ALU.sv
+++ lab2_final/ALU.sv
@@ -4,7 +4,7 @@ module ALU(ALU_inA, ALU_control, ALU_inB, zero, ALU_result);
 	//assign inputs and outputs
 	input logic [31:0] ALU_inA, ALU_inB;
 	input logic [3:0] ALU_control;
-	output logic signed [31:0] ALU_result;
+	output logic [31:0] ALU_result;
 	output logic zero;
 	
 	// based on ALU control select the ALUs operation
