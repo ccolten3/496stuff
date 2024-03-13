784b0f4af9a2030a25ff3892235e41fc9812547e
AlexOv77 <122645914+AlexOv77@users.noreply.github.com>
2024-03-12 23:48:20 -0700

    Update f2_dec_preg.sv

--- lab2_final/f2_dec_preg.sv
+++ lab2_final/f2_dec_preg.sv
@@ -1,8 +1,10 @@
+// f2 decoding register
 module f2_dec_preg(clk, reset, PC_out_f2, PC4_f2, stall, PC_out_dec, PC4_dec);
 	input logic clk, reset, stall;
 	input logic [31:0] PC_out_f2, PC4_f2;
 	output logic [31:0] PC_out_dec, PC4_dec;
-	
+
+	// registers on pos clock cycle
 	always @(posedge clk) begin
 		if (reset) begin
 			PC_out_dec <= 32'h0;
