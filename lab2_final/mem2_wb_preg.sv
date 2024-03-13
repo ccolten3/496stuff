f0b71c682299171e02124e44bb2d4a43b979658b
AlexOv77 <122645914+AlexOv77@users.noreply.github.com>
2024-03-12 23:50:31 -0700

    Update mem2_wb_preg.sv

--- lab2_final/mem2_wb_preg.sv
+++ lab2_final/mem2_wb_preg.sv
@@ -1,4 +1,5 @@
 `include "riscv32_starter.sv"
+// register for mem2 and writeback pipelining
 module mem2_wb_preg(clk, reset,stall, control_mem2, PC4_mem2, ALU_mem2, write_reg_mem2, instr_mem2, 
 										control_wb, PC4_wb, ALU_wb, write_reg_wb, instr_wb);
 	input logic clk, reset, stall;
@@ -7,7 +8,8 @@ module mem2_wb_preg(clk, reset,stall, control_mem2, PC4_mem2, ALU_mem2, write_re
 	
 	output logic [31:0] control_wb, PC4_wb, ALU_wb, instr_wb;
 	output logic [4:0] write_reg_wb;
-	
+
+	// registers on pos clock cycle
 	always @(posedge clk) begin
 		control_wb <= control_mem2;
 		instr_wb <= instr_mem2;
