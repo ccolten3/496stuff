44245e904110eea544b21d0245e0a2dbf948aa5a
AlexOv77 <122645914+AlexOv77@users.noreply.github.com>
2024-03-12 23:47:52 -0700

    Update f1_f2_preg.sv

--- lab2_final/f1_f2_preg.sv
+++ lab2_final/f1_f2_preg.sv
@@ -1,9 +1,10 @@
+// register for f1 and f2
 module f1_f2_preg(clk, reset, PC4_f1, PC_out_f1, PC4_f2, PC_out_f2);
 	input logic clk, reset;
 	input logic [31:0] PC4_f1, PC_out_f1;
 	output logic [31:0] PC4_f2, PC_out_f2;
 
-	
+	// registers on the clock cycle
 	always @(posedge clk) begin 
 		if (reset) begin 
 			PC4_f2 <= 32'h0;
